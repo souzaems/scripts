6ebb34cf60a2df32150b82d5a54c1976a54a3cbb
Érica Souza <43590100+souzaems@users.noreply.github.com>
2019-09-07 21:18:22 -0300

    using_RepeatMasker.py

---
+++ using_RepeatMasker.py
@@ -0,0 +1,10 @@
+##Looking for repeat elements using RepeatMasker
+#Construct the database
+/dados/software/RepeatModeler-open-1.0.11/BuildDatabase -name NOME.db -engine ncbi ../caminho_scaffolds/nome.scaffolds/fasta
+
+#We need to run RepeatModeler due the fact that we don't have trasncriptome data to use in the next steps
+/dados/software/RepeatModeler-open-1.0.11/RepeatModeler -database NOME.db -engine ncbi -pa 30
+
+#After this step we need to have a file NAME.fa.classified to run RepeatMasker showing the repeat regions
+nohup /dados/software/RepeatMasker/RepeatMasker -pa 30 -xsmall -gff -lib RM_caminhopasta/consensis.fa.classified ../caminhoscaffold/scaffold.fasta&
+
